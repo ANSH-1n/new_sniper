22f9a228b32a34b6a018c92001ecb096d7adbbbb
anshjamwal108 <anshjamwal108@gmail.com>
2025-05-14 10:29:31 +0530

    after fixing the packages error

--- components/ParallaxSection.tsx
+++ components/ParallaxSection.tsx
@@ -1,55 +1,5 @@
 
 
-// // components/ParallaxSection.tsx
-// 'use client';
-
-// import { MouseParallax } from "react-just-parallax";
-// import React from "react";
-
-// const ParallaxSection: React.FC = () => {
-//   return (
-//     <MouseParallax strength={0.1}>
-//       {/* Add content here if needed */}
-//     </MouseParallax>
-//   );
-// };
-
-// export default ParallaxSection;
-
-
-
-
-
-
-
-
-
-
-// // components/ParallaxSection.tsx
-// 'use client';
-
-// import { MouseParallax } from "react-just-parallax";
-
-// const ParallaxSection: React.FC = () => {
-//   return (
-//     <MouseParallax strength={0.1}>
-//       {/* Add content here if needed */}
-//     </MouseParallax>
-//   );
-// };
-
-// export default ParallaxSection;
-
-
-
-
-
-
-
-
-
-
-
 'use client';
 
 import { useEffect, useState } from 'react';
